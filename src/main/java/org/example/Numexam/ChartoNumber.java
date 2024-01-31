ac859bff42b42f3bf5247b933aaeca0571fa9e48
NanNo <inhwanpark19991120@gmail.com>
2024-01-31 21:32:40 +0900

    Java Char_TO_Number

---
+++ src/main/java/org/example/Numexam/ChartoNumber.java
@@ -0,0 +1,11 @@
+package org.example.Numexam;
+
+public class ChartoNumber {
+    public static void main(String[] args){
+        char c1 = 'a';
+        System.out.println((int)c1);
+
+        char c2 = (char)97;
+        System.out.println(c2);
+    }
+}
