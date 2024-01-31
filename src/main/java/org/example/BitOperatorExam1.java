66db0e97a5a8deb33c48a6550d1aeda4a0880bd9
NanNo <inhwanpark19991120@gmail.com>
2024-01-31 22:31:48 +0900

    BitOperatorExam

---
+++ src/main/java/org/example/BitOperatorExam1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class BitOperatorExam1 {
+    public static void main(String[] args){
+
+        int a = 4;
+        int b = a >> 1; // 2^1 으로 나눈다.
+        System.out.println(b);
+
+        int c = 4;
+        int d = c << 1; //2^1 으로 곱한다.
+        System.out.println(d);
+
+        int e = 10;
+        int f = e >>> 1;
+        System.out.println(f);
+    }
+}
+
