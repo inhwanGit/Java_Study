d7716754dd5cc1d93c1c6a7dd317302a74f8daf0
NanNo <inhwanpark19991120@gmail.com>
2024-02-01 23:46:14 +0900

    class

---
+++ src/main/java/org/example/Vendingmachine/VendingMachine.java
@@ -0,0 +1,5 @@
+package org.example.Vendingmachine;
+
+public class VendingMachine {
+
+}
---
+++ src/main/java/org/example/Vendingmachine/VendingMachineMain.java
@@ -0,0 +1,10 @@
+package org.example.Vendingmachine;
+
+public class VendingMachineMain {
+
+    public static void main(String[] args) {
+
+        VendingMachine machine = new VendingMachine();
+
+    }
+}
