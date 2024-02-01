1b771b80284cd5d3a9493315b94a13a7975e6e66
NanNo <inhwanpark19991120@gmail.com>
2024-02-01 17:38:50 +0900

    ForExam

---
+++ src/main/java/org/example/For/ForExam2.java
@@ -0,0 +1,11 @@
+package org.example.For;
+
+public class ForExam2 {
+    public static void main(String[] args){
+       for (int k = 2; k <= 3; k++){
+           for(int i = 1; i <=9; i++){
+               System.out.println(k+"*"+i+"="+(k * i));
+           }
+       }
+    }
+}
