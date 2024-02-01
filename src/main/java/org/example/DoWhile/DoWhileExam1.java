77579e6be55cfc78847f5abe192e578fc660c3ba
NanNo <inhwanpark19991120@gmail.com>
2024-02-01 13:19:40 +0900

    DoWhileExam

---
+++ src/main/java/org/example/DoWhile/DoWhileExam1.java
@@ -0,0 +1,11 @@
+package org.example.DoWhile;
+
+public class DoWhileExam1 {
+    public static void main(String[] args){
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
+    }
+}
