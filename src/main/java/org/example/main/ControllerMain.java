78e1e0919efe31c52a472d5ab4b49afe72acd2cb
NanNo <inhwanpark19991120@gmail.com>
2024-02-03 13:09:49 +0900

    main

---
+++ src/main/java/org/example/main/ControllerMain.java
@@ -0,0 +1,11 @@
+package org.example.main;
+
+import 토요일.fw.Controller;
+import 토요일.myproject.FirstController;
+
+public class ControllerMain {
+    public static void main(String[] args) {
+        Controller controller = new FirstController();
+        controller.execute();
+    }
+}
