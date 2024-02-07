d99f58957d3b712068b9c179a4d4aadfe0c9e0d1
NanNo <inhwanpark19991120@gmail.com>
2024-02-07 15:08:25 +0900

    InputSteam01

--- src/main/java/org/example/IO/InputStream01.java
+++ src/main/java/org/example/IO/InputStream01.java
@@ -1,4 +1,20 @@
 package org.example.IO;
 
+import java.io.InputStream;
+
 public class InputStream01 {
+    public static void main(String[] args) {
+        InputStream in = null;
+        try{
+            int data = in.read(); // byte 단위로 읽어 들여오는 read메소드가 byte가 아닌 int를 리턴하는 이유 -> EOF를 표현할 수 있는 방법이 없어서
+        }catch (Exception ex1){
+            System.out.println(ex1);
+        }finally {
+            try {
+                in.close();
+            }catch (Exception ex2){
+                System.out.println(ex2);
+            }
+        }
+    }
 }
