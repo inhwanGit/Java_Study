63137a2892398cc4f8348c806645a86b7e83f803
NanNo <inhwanpark19991120@gmail.com>
2024-02-09 00:45:18 +0900

    회원관리

--- src/main/java/org/example/UserControl/UserMain.java
+++ src/main/java/org/example/UserControl/UserMain.java
@@ -13,11 +13,10 @@ public class UserMain {
                 case 1 : {User user = new UserUI().regMenu(users); users.add(user);
                     System.out.println("회원등록이 완료되었습니다."); break;}
                 case 2 : {userUI.printUser(users); break;}
-                case 3 : {
-                    System.out.println("프로그램을 종료합니다.");
-                    userDao.saveUser(users);
-                    System.exit(0);
-                }
+                case 3 : {userUI.updateUser(users); break;}
+                case 4 : {userUI.deleteUser(users); break;}
+                case 5 : {userDao.saveUser(users);
+                    System.out.println("시스템을 종료합니다."); System.exit(0);}
                 default: {
                     System.out.println("키를 잘못 입력하셨습니다.");
                 }
--- src/main/java/org/example/UserControl/UserUI.java
+++ src/main/java/org/example/UserControl/UserUI.java
@@ -3,7 +3,6 @@ package org.example.UserControl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserUI {
@@ -22,7 +21,7 @@ public class UserUI {
         System.out.print("4. 회원삭제");
         System.out.print("      ");
         System.out.println("0. 종료");
-        System.out.println("=======================================================");
+        System.out.println("====================================================================");
         System.out.print("이용하실 메뉴를 입력해주세요 : ");
         int menuId = -1;
         try {
@@ -41,7 +40,6 @@ public class UserUI {
             System.out.print("생일을 입력하세요(숫자만 입력하세요) : ");
             String strbirthday = br.readLine();
             String email;
-
             while (true){
                 System.out.print("이메일을 입력하세요 : ");
                 email = br.readLine();
@@ -49,7 +47,7 @@ public class UserUI {
                 // 이메일 중복확인
                 for(User existingUser : users){
                     if(existingUser.getEmail().equals(email)){
-                        System.out.println("이미 존재하는 이메일입니다.");
+                        System.out.print("이미 존재하는 이메일입니다. 다시 입력해주세요");
                         exitwhile = true;
                         break;
                     }
@@ -59,8 +57,6 @@ public class UserUI {
                     break;
                 }
             }
-
-
             int birthday = Integer.parseInt(strbirthday);
             User user = new User(name,birthday,email);
             return user;
@@ -84,8 +80,69 @@ public class UserUI {
         }
     }
 
-    //"3. 회원정보수정"
-    public void updateUser(List<User> users){
-        //1. 이메일로 회원 찾기
+    // "3. 회원정보수정"
+    public User updateUser(List<User> users) {
+        try {
+            System.out.println("수정할시 회원님의 이메일을 입력해주세요.");
+            String email = br.readLine();
+
+            for (User serachUser : users) {
+                if (serachUser.getEmail().equals(email)) {
+                    System.out.println("1. 회원 이름  2. 회원 생년월일  3. 회원 이메일");
+                    System.out.println("=======================================");
+                    System.out.print("수정하실 기능을 입력해주세요 : ");
+                    String strNum = br.readLine();
+                    int num = Integer.parseInt(strNum);
+
+                    if (num == 1) {
+                        System.out.print("수정할시 회원님의 이름을 입력해주세요. : ");
+                        String updateName = br.readLine();
+                        serachUser.setName(updateName);
+                    } else if(num == 2){
+                        System.out.print("수정할시 회원님의 생년월일 입력해주세요. : ");
+                        String updateBirthday = br.readLine();
+                        int intUpdateBirthday = Integer.parseInt(updateBirthday);
+                        serachUser.setBirthday(intUpdateBirthday);
+                    } else if(num == 3){
+                        System.out.print("수정할시 회원님의 이메일 입력해주세요. : ");
+                        String updateEmail = br.readLine();
+                        serachUser.setName(updateEmail);
+                    } else {
+                        System.out.println("잘못된 입력입니다. 수정을 종료합니다.");
+                        break;
+                    }
+                    System.out.println("수정이 완료되었습니다.");
+                    return serachUser;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    // "4. 회원삭제"
+    public User deleteUser(List<User> users){
+        try {
+            System.out.print("삭제하실 회원님의 이메일을 입력해주세요. : ");
+            String email = br.readLine();
+            //1. 이메일로 회원 찾기
+            User foundUser = null;
+            for (User user : users) {
+                if (user.getEmail().equals(email)) {
+                    //찾은 회원 정보 담기
+                    foundUser = user;
+                    break;
+                }
+            }
+            if(foundUser != null){
+                users.remove(foundUser);
+                System.out.println("회원 정보를 삭제하였습니다.");
+            }else {
+                System.out.println("해당 이메일은 존재하지 않습니다.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 }
