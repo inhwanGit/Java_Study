59e4f07fe9c30d29682f918a3105e34782274287
NanNo <inhwanpark19991120@gmail.com>
2024-02-11 12:57:56 +0900

    WebServer

--- src/main/java/org/example/WebServer/WebServer.java
+++ src/main/java/org/example/WebServer/WebServer.java
@@ -14,44 +14,63 @@ public class WebServer {
                 Socket clientSocket = serverSocket.accept(); // 대기한다. 클라이언트가 접속하면 클라이언트와 통신하는 Socket이 반환된다.
                 System.out.println("2");
 
-                InputStream inputStream = clientSocket.getInputStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-                OutputStream outputStream = clientSocket.getOutputStream();
-                PrintWriter pw = new PrintWriter(new OutputStreamWriter(outputStream));
-
-                // http://localhost:1000/hello
-                // GET /hello HTTP/1.1
-                String firstLine = br.readLine();
-                String msg = "";
-                if (firstLine.indexOf("/hello") >= 0)
-                    msg = "hi";
-                else if (firstLine.indexOf("/hi") >= 0)
-                    msg = "hi";
-                System.out.println(firstLine);
-
-                String line = null;
-                while (!(line = br.readLine()).equals("")) {
-                    System.out.println(line);
-                }
-                // 빈줄까지 읽어들이면 끝
-
-
-                pw.println("HTTP/1.1 200 OK");
-                pw.println("name:kim");
-                pw.println("email:inhwna@naver.com");
-                pw.println();
-                pw.println("<html>");
-                pw.println(msg + "world!!");
-                pw.println("</html>");
-
-                pw.flush(); // 서버에게 전송
-                br.close();
-                pw.close();
-                clientSocket.close();
+                ClientThread ct = new ClientThread(clientSocket);
+                ct.start();
             }
         }finally {
             serverSocket.close();
         }
+    }
+
+}
+
+class ClientThread extends Thread{
+    private Socket clientSocket;
+    public ClientThread(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+
+
+    public void run(){
+        try {
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+            OutputStream outputStream = clientSocket.getOutputStream();
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(outputStream));
+
+            // http://localhost:1000/hello
+            // GET /hello HTTP/1.1
+            String firstLine = br.readLine();
+            String msg = "";
+            if (firstLine.indexOf("/hello") >= 0)
+                msg = "hi";
+            else if (firstLine.indexOf("/hi") >= 0)
+                msg = "hi";
+            System.out.println(firstLine);
+
+            String line = null;
+            while (!(line = br.readLine()).equals("")) {
+                System.out.println(line);
+            }
+            // 빈줄까지 읽어들이면 끝
+
+
+            pw.println("HTTP/1.1 200 OK");
+            pw.println("name:kim");
+            pw.println("email:inhwna@naver.com");
+            pw.println();
+            pw.println("<html>");
+            pw.println(msg + "world!!");
+            pw.println("</html>");
+
+            pw.flush(); // 서버에게 전송
+            br.close();
+            pw.close();
+            clientSocket.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
     }
 }
