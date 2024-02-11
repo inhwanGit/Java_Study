e672753be2d69ee3eedbe096db1634c41e0848ad
NanNo <inhwanpark19991120@gmail.com>
2024-02-11 12:30:34 +0900

    WebServer

--- src/main/java/org/example/WebServer/WebServer.java
+++ src/main/java/org/example/WebServer/WebServer.java
@@ -1,5 +1,8 @@
 package org.example.WebServer;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,5 +13,12 @@ public class WebServer {
         System.out.println("1");
         Socket clientSocket = serverSocket.accept(); // 대기한다. 클라이언트가 접속하면 클라이언트와 통신하는 Socket이 반환된다.
         System.out.println("2");
+
+        InputStream inputStream = clientSocket.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+        // http://localhost:1000/hello
+        // GET /hello HTTP/1.1
+        System.out.println(br.readLine());
     }
 }
